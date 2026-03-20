bd3d96f26b5c62b71ada64e6c80579a4660d938c
Ryan Kubetz <rkubetz@charlotte.edu>
2026-03-19 22:58:58 -0400

    feat: add PUT request endpoint for Part 2

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item-name": item.name, "item_id": item_id}
